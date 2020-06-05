e9c4432cb00012c1d6b7f54fda577b228ff49c64
Yo-yuya <soroguzugosa@gmail.com>
2020-06-05 23:52:30 +0900

    count

--- src/mian.cpp
+++ src/mian.cpp
@@ -1,18 +1,21 @@
-﻿char mojiretsu[1000];
-int mojisuu = 0;
+﻿/* 問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する */
+
 /* header Files */
 #include <stdio.h>
 #include <stdlib.h>
 
+char mojiretsu[1000];
+int mojisuu = 0;
+int count = 1000;
 /*  */
 #define _CRT_SECURE_NO_WARNINGS
 #ifdef this_is_a_great_codo
-問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
+
 #endif
 /* main */
 int main() { 
-    while (mojisuu++ < 1000) 
+    while (mojisuu++ < count) 
     sprintf(mojiretsu, "%d", mojisuu), 
-     printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); 
+     printf("%s, ", mojisuu % 15 ?  mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); 
      system("PAUSE"); 
      }
