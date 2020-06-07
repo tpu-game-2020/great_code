b06ef197de6c8d55aaa0343319273e85db0cbdc0
Yo-yuya <soroguzugosa@gmail.com>
2020-06-07 16:38:15 +0900

    コメントアウト変更

--- src/mian.cpp
+++ src/mian.cpp
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//char mojiretsu[1000];
-//int mojisuu = 0;
-//int count = 1000;
 
 /*  */
 #define _CRT_SECURE_NO_WARNINGS
@@ -19,12 +16,11 @@ int main()
  { 
     //while (mojisuu++ < count) 
     for (int i = 1; i < MAXCOUNT; i++)
-    {
-        //sprintf(mojiretsu, "%d", mojisuu); 
-        if (i % 15 == 0) printf("FizzBuzz, ");
-        else if (i % 5 == 0) printf("Buzz, ");
-        else if (i % 3 == 0) printf("Fizz, ");
-        else printf("%d, ",i);
+    {   int mojisu = i;
+        if (mojisu % 15 == 0) printf("FizzBuzz, ");
+        else if (mojisu % 5 == 0) printf("Buzz, ");
+        else if (mojisu % 3 == 0) printf("Fizz, ");
+        else printf("%d, ",mojisu);
     }
     system("PAUSE"); 
 }
