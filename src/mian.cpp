5984b658199a2b2b61282d896ae2736786fe94d8
Yo-yuya <soroguzugosa@gmail.com>
2020-06-07 16:19:27 +0900

    for文

--- src/mian.cpp
+++ src/mian.cpp
@@ -4,21 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*変数設定 */
-char mojiretsu[1000];
-int mojisuu = 0;
-int count = 1000;
+//char mojiretsu[1000];
+//int mojisuu = 0;
+//int count = 1000;
 
 /*  */
 #define _CRT_SECURE_NO_WARNINGS
+#define MAXCOUNT 1000
 #ifdef this_is_a_great_codo
 
 #endif
 /* main */
 int main() { 
-    while (mojisuu++ < count) 
-          sprintf(mojiretsu, "%d", mojisuu), 
-          printf("%s, ", mojisuu % 15 ?  mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); 
-
-     system("PAUSE"); 
-     }
+    //while (mojisuu++ < count) 
+    for (int i = 0; i < MAXCOUNT; i++)
+    {
+        //sprintf(mojiretsu, "%d", mojisuu); 
+        if (i % 15 == 0) printf("FizzBuzz");
+        else if (i % 5 == 0) printf("Buzz");
+        else if (i % 3 == 0) printf("Fizz");
+        else printf("%d, ",i);
+    }
+    system("PAUSE"); 
+    // }
